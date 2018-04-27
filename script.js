17363f4d78157a1eca301b0a3886becd8cf89237
Sabine Gruber <sab@vollbio.de>
2018-04-28 01:38:45 +0200

    stylesheets

--- script.js
+++ script.js
@@ -3,20 +3,16 @@ function calculateInsurance() {
     var y = document.getElementById("horsepower").value;
     var z = document.getElementById("country");
     if (z == "Austria") {
-    	var insurance = y * 100 / x  + 50;
-    	return insurance;
+        var insurance = y * 100 / x + 50;
+        return insurance;
 	} else if (z == "Germany") {
-		var insurance = y * 120 / x  + 100;
-	} else if (z == "Italy") {
-		var insurance = y * 150 / (x + 2)  + 50;
-	} else {
-		console.log("Not available in your country");
-	
-	}
-console.log(insurance);
-	
+        var insurance = y * 120 / x + 100;
+        return insurance;
+    } else if (z == "Italy") {
+        var insurance = y * 150 / (x + 2) + 50;
+        return insurance;
+    } else {
+        console.log("Not available in your country");
+    }
+        console.log("Insurance = " + insurance);
 }
-    /*var insurance = y * 100 / x  + 50;
-    document.write(insurance);
-} */
-
