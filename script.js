10ed6fd46165498410f25f41d433780eea421868
Sabine Gruber <sab@vollbio.de>
2018-04-28 02:49:36 +0200

    function calculateInsurance

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function calculateInsurance() {
     var x = document.getElementById("age").value;
     var y = document.getElementById("horsepower").value;
-    var z = document.getElementById("country");
-    if (z == "Austria") {
+    var z = document.getElementById("country").value;
+    if (z === "Austria") {
         var insurance = y * 100 / x + 50;
-        return insurance;
-	} else if (z == "Germany") {
+        
+	} else if (z === "Germany") {
         var insurance = y * 120 / x + 100;
-        return insurance;
-    } else if (z == "Italy") {
+        
+    } else if (z === "Italy") {
         var insurance = y * 150 / (x + 2) + 50;
-        return insurance;
+        
     } else {
-        console.log("Not available in your country");
+        //console.log("Not available in your country");
+        //document.getElementById("printcal").innerHTML = "Not available in your country";
     }
-        console.log("Insurance = " + insurance);
+        //console.log(insurance);
+        //document.getElementById("printcal").innerHTML = insurance;
+        document.write(insurance);
 }
+
